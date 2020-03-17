9281278a888a76b0063dd8d2b4c87371259a5173
razvan andrei <razvan.andrei.lismanu@everis.com>
2020-03-17 17:16:28 +0100

    firstcommit

---
+++ Test Suites/Prueba.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Prueba</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e65ba81c-0254-47ec-a0be-38e37f7edcd8</testSuiteGuid>
+   <testCaseLink>
+      <guid>98c21f22-a5d1-4660-8fe9-d464623ed066</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/prueba</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
